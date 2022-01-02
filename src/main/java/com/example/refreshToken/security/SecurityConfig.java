aafbcd9aed4bec813a75bc70c0dae18e1b10830e
msrecec <67606889+msrecec@users.noreply.github.com>
2022-01-02 16:18:57 +0100

    Implemented access token authorization

--- src/main/java/com/example/refreshToken/security/SecurityConfig.java
+++ src/main/java/com/example/refreshToken/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.refreshToken.security;
 
 import com.example.refreshToken.filter.CustomAuthenticationFilter;
+import com.example.refreshToken.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -43,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
